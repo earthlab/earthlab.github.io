87bdfde5f1a8a9f9817d945e3ebdc04b0d725aac
Max Joseph <maxwellbjoseph@gmail.com>
2016-12-09 11:39:31 -0700

    including support for Rmd files in make build

--- processing-code/generate_posts.R
+++ processing-code/generate_posts.R
@@ -3,9 +3,9 @@ library(tools)
 library(nbconvertR)
 library(yaml)
 library(rlist)
+library(rmarkdown)
 
 # Find files that need to be converted ------------------------------------
-
 find_files_to_convert <- function(input_dir) {
   extensions <- c("ipynb", "Rmd", "md")
   patt <- paste0("\\.(", paste(extensions, collapse = "|"), ")$")
@@ -26,6 +26,7 @@ files_to_convert <- find_files_to_convert("tutorials")
 # Convert the files to md posts -------------------------------------------
 
 make_post <- function(file, dest_dir = "_posts") {
+  
   # first, generate an output filename, e.g., _posts/R/2015-01-22-test.md
   ext <- file_ext(file)
   md_out <- gsub(paste0("\\.", ext), ".md", file)
@@ -37,8 +38,28 @@ make_post <- function(file, dest_dir = "_posts") {
   dir_out <- file.path(dest_dir, sub_dir)
 
   if (ext == "Rmd") {
+    rmarkdown::render(file, 
+                      output_format = "md_document", 
+                      output_dir = dir_out)
     
-    # TODO: adapt Leah's code to make posts from Rmd files
+    md_filename <- file.path(dir_out, md_name)
+    yaml_list <- yaml_from_md(file)
+    post_date <- yaml_list$date
+    
+    # prepend the date to the markdown file name
+    dated_md_filename <- file.path(dir_out, 
+                                   paste(post_date, md_name, 
+                                         sep = "-"))
+    file.rename(md_filename, dated_md_filename)
+    
+    # add the yaml frontmatter back into the md file
+    post_text <- readLines(dated_md_filename)
+    post_text <- c("---", 
+                   as.yaml(yaml_list) %>% strsplit(split = "\n") %>% unlist(), 
+                   "---", 
+                   post_text)
+    # move images to right directory & update paths in post
+    move_images(dated_md_filename, md_filename, md_name, post_text, Rmd = TRUE)
     
   } else if (ext == "ipynb") {
     cmd <- paste("jupyter nbconvert --to markdown --output-dir", dir_out, file)
@@ -63,26 +84,9 @@ make_post <- function(file, dest_dir = "_posts") {
                                    paste(post_date, md_name, 
                                          sep = "-"))
     file.rename(md_filename, dated_md_filename)
-
-    image_dir <- gsub("\\.md", replacement = "_files", x = md_filename)
-    if (dir.exists(image_dir)) {
-      # # move the images to the right directory
-      new_image_dir <- file.path("images", 
-                                 gsub("\\.md", 
-                                      replacement = "_files", 
-                                      x = md_name))
-      file.remove(list.files(new_image_dir, full.names = TRUE))
-      file.remove(new_image_dir)
-      file.rename(image_dir, new_image_dir)
-      
-      # fix the file paths in the actual post
-      old_image_path <- unlist(strsplit(new_image_dir, split = .Platform$file.sep))[2]
-      new_image_path <- paste0(.Platform$file.sep, new_image_dir)
-      post_text <- gsub(old_image_path, 
-                        new_image_path, 
-                        post_text)
-      writeLines(post_text, dated_md_filename)
-    }
+    
+    # move images to right directory & update paths in post
+    move_images(dated_md_filename, md_filename, md_name, post_text)
     
   } else if (ext == "md") {
     # extract date from yaml
@@ -103,6 +107,45 @@ make_post <- function(file, dest_dir = "_posts") {
 }
 
 
+move_images <- function(dated_md_filename, md_filename, md_name, post_text, 
+                        Rmd = FALSE) {
+  if (Rmd) {
+    image_dir <- gsub("\\.md", replacement = "_files/figure-markdown_strict", 
+                      x = md_filename)
+  } else {
+    image_dir <- gsub("\\.md", replacement = "_files", x = md_filename)
+  }
+  if (dir.exists(image_dir)) {
+    # move the images to the right directory
+    new_image_dir <- file.path("images", 
+                               gsub("\\.md", 
+                                    replacement = "_files", 
+                                    x = md_name))
+    if (dir.exists(new_image_dir)) {
+      file.remove(list.files(new_image_dir, full.names = TRUE))
+      file.remove(new_image_dir)
+    }
+    file.rename(image_dir, new_image_dir)
+    parent_img_dir <- gsub("\\.md", replacement = "_files", x = md_filename)
+    if (dir.exists(parent_img_dir)) {
+      file.remove(parent_img_dir)
+    }
+    
+    # fix the file paths in the actual post
+    if (Rmd) {
+      old_image_path <- file.path(getwd(), image_dir)
+    } else {
+      old_image_path <- unlist(strsplit(new_image_dir, split = .Platform$file.sep))[2]
+    }
+    new_image_path <- paste0(.Platform$file.sep, new_image_dir)
+    post_text <- gsub(old_image_path, 
+                      new_image_path, 
+                      post_text)
+    writeLines(post_text, dated_md_filename)
+  }
+}
+
+
 yaml_from_md <- function(md_filename){
   # extract the yaml frontmatter from md file
   # returns an R list
@@ -114,6 +157,8 @@ yaml_from_md <- function(md_filename){
   yaml_list <- list.parse(paste(frontmatter, collapse = "\n"), "yaml")
   yaml_list
 }
+
+
 
 pb <- txtProgressBar(min = 1, max = length(files_to_convert), style = 3)
 for (i in seq_along(files_to_convert)) {
