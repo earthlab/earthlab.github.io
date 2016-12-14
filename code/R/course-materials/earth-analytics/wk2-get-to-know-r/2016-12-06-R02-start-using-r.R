1e621e73c4c590f23f962b4f9f4f45a52bad9092
Leah Wasser <leah.wasser@colorado.edu>
2016-12-14 15:27:43 -0700

    updated code from build

--- code/R/course-materials/earth-analytics/wk2-get-to-know-r/2016-12-06-R02-start-using-r.R
+++ code/R/course-materials/earth-analytics/wk2-get-to-know-r/2016-12-06-R02-start-using-r.R
@@ -1,15 +1,7 @@
-## ----example-script------------------------------------------------------
+## ------------------------------------------------------------------------
+3 + 5
+12/7
 
-# let's define some variables
-my.variable <- (1*2) + 5
-my.second.variable <- 7 * 3
-
-# I can sum two variables together using the sum function
-sum(my.variable, my.second.variable, na.rm=FALSE)
-
-
-## ---- results='show'-----------------------------------------------------
-
-round(3.14159)
-
+## ------------------------------------------------------------------------
+weight_kg <- 55
 
