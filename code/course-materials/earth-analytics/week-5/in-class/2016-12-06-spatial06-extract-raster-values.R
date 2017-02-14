b56387e8c5829fc4e5a995702482557933958591
Leah Wasser <leah.wasser@colorado.edu>
2017-02-14 16:35:18 -0700

    edits to week 5

--- code/course-materials/earth-analytics/week-5/in-class/2016-12-06-spatial06-extract-raster-values.R
+++ code/course-materials/earth-analytics/week-5/in-class/2016-12-06-spatial06-extract-raster-values.R
@@ -10,9 +10,9 @@ options(stringsAsFactors = FALSE)
 # set working directory
 # setwd("path-here/earth-analytics")
 
-## ----import-chm, fig.cap="histogram of CHM values"-----------------------
+## ----import-chm, fig.cap="Histogram of CHM values"-----------------------
 # import canopy height model (CHM).
-SJER_chm <- raster("data/week4//california/SJER/2013/lidar/SJER_lidarCHM.tif")
+SJER_chm <- raster("data/week5/california/SJER/2013/lidar/SJER_lidarCHM.tif")
 SJER_chm
 
 # plot the data
@@ -29,16 +29,15 @@ SJER_chm[SJER_chm==0] <- NA
 
 # plot the modified data
 hist(SJER_chm,
-     main="Histogram of Canopy Height\n NEON SJER Field Site, 0 values set to NA",
+     main="Histogram of Canopy Height\n pixels==0 set to NA",
      col="springgreen",
      xlab="Height (m)")
 
 
 ## ----read-plot-data, fig.cap="canopy height model / plot locations plot"----
 # import plot centroids
-SJER_plots <- readOGR("data/week4/california/SJER/vector_data",
+SJER_plots <- readOGR("data/week5/california/SJER/vector_data",
                       "SJER_plot_centroids")
-
 
 # Overlay the centroid points and the stem locations on the CHM plot
 plot(SJER_chm,
@@ -54,9 +53,8 @@ plot(SJER_plots,
 
 
 ## ----extract-plot-data---------------------------------------------------
-
 # Insitu sampling took place within 40m x 40m square plots, so we use a 20m radius.
-# Note that below will return a dataframe containing the max height
+# Note that below will return a data.frame containing the max height
 # calculated from all pixels in the buffer for each plot
 SJER_height <- extract(SJER_chm,
                     SJER_plots,
@@ -67,7 +65,6 @@ SJER_height <- extract(SJER_chm,
 
 
 ## ----explore-data-distribution, eval=FALSE-------------------------------
-## 
 ## # cent_ovrList <- extract(chm,centroid_sp,buffer = 20)
 ## # create histograms for the first 5 plots of data
 ## # for (i in 1:5) {
@@ -76,9 +73,8 @@ SJER_height <- extract(SJER_chm,
 ## 
 
 ## ----unique-plots--------------------------------------------------------
-
 # import the centroid data and the vegetation structure data
-SJER_insitu <- read.csv("data/week4/california/SJER/2013/insitu/veg_structure/D17_2013_SJER_vegStr.csv",
+SJER_insitu <- read.csv("data/week5/california/SJER/2013/insitu/veg_structure/D17_2013_SJER_vegStr.csv",
                         stringsAsFactors = FALSE)
 
 # get list of unique plots
@@ -86,21 +82,16 @@ unique(SJER_plots$Plot_ID)
 
 
 ## ----analyze-plot-dplyr--------------------------------------------------
-
-# find the max and averagestem height for each plot
+# find the max and mean stem height for each plot
 insitu_stem_height <- SJER_insitu %>%
   group_by(plotid) %>%
   summarise(insitu_max = max(stemheight), insitu_avg = mean(stemheight))
 
-head(insitu_stem_height)
-
-# let's create better, self documenting column headers
-names(insitu_stem_height) <- c("plotid", "insituMaxHt")
+# view the data frame to make sure we're happy with the column names.
 head(insitu_stem_height)
 
 
 ## ----merge-dataframe-----------------------------------------------------
-
 # merge the insitu data into the centroids data.frame
 SJER_height <- merge(SJER_height,
                      insitu_stem_height,
@@ -122,31 +113,44 @@ plot(SJER_height,
      cex=(SJER_height$SJER_lidarCHM)/10, # size symbols according to tree height attribute normalized by 10
      add=T)
 
-legend('bottomright',
-       legend="plot location \nsized by tree height",
+# place legend outside of the plot
+par(xpd=T)
+legend(SJER_chm@extent@xmax+250, 
+       SJER_chm@extent@ymax,
+       legend="plot location \nsized by \ntree height",
        pch=19,
        bty='n')
 
 
 ## ----plot-w-ggplot, fig.cap="ggplot - measured vs lidar chm."------------
-
 # create plot
-ggplot(SJER_height@data, aes(x=SJER_lidarCHM, y = insituMaxHt)) +
+ggplot(SJER_height@data, aes(x=SJER_lidarCHM, y = insitu_avg)) +
   geom_point() +
   theme_bw() +
-  ylab("Maximum measured height") +
-  xlab("Maximum LiDAR pixel")+
-  geom_abline(intercept = 0, slope=1) +
-  ggtitle("Lidar Height Compared to InSitu Measured Height")
+  ylab("Mean measured height") +
+  xlab("Mean LiDAR pixel") +
+  ggtitle("Lidar Derived Mean Tree Height \nvs. InSitu Measured Mean Tree Height (m)")
+
+
+## ----plot-w-ggplot2, fig.cap="ggplot - measured vs lidar chm w one to one line."----
+# create plot
+ggplot(SJER_height@data, aes(x=SJER_lidarCHM, y = insitu_avg)) +
+  geom_point() +
+  theme_bw() +
+  ylab("Mean measured height") +
+  xlab("Mean LiDAR pixel") +
+  xlim(0,15) + ylim(0,15) + # set x and y limits to 0-20
+  geom_abline(intercept = 0, slope=1) + # add one to one line
+  ggtitle("Lidar Derived Tree Height \nvs. InSitu Measured Tree Height")
 
 
 ## ----ggplot-data, fig.cap="Scatterplot measured height compared to lidar chm."----
-
 # plot with regression fit
-p <- ggplot(SJER_height@data, aes(x=SJER_lidarCHM, y = insituMaxHt)) +
+p <- ggplot(SJER_height@data, aes(x=SJER_lidarCHM, y = insitu_avg)) +
   geom_point() +
   ylab("Maximum Measured Height") +
   xlab("Maximum LiDAR Height")+
+    xlim(0,15) + ylim(0,15) + # set x and y limits to 0-20
   geom_abline(intercept = 0, slope=1)+
   geom_smooth(method=lm)
 
@@ -158,27 +162,16 @@ p + theme(panel.background = element_rect(colour = "grey")) +
 
 
 ## ----view-diff, fig.cap="box plot showing differences between chm and measured heights."----
+# Calculate difference
+SJER_height@data$ht_diff <-  (SJER_height@data$SJER_lidarCHM - SJER_height@data$insitu_avg)
 
-SJER_height@data$ht_diff <-  (SJER_height@data$SJER_lidarCHM - SJER_height@data$insituMaxHt)
-
-# base plot example below
-# barplot(SJER_height@data$ht_diff,
-#        xlab = SJER_height@data$Plot_ID)
 
 
 # create bar plot using ggplot()
-ggplot(data=SJER_height@data, aes(x=Plot_ID, y=ht_diff, fill=Plot_ID)) +
-    geom_bar(stat="identity")
+ggplot(data=SJER_height@data, 
+       aes(x=Plot_ID, y=ht_diff, fill=Plot_ID)) +
+       geom_bar(stat="identity") +
+       xlab("Plot Name") + ylab("Height difference (m)")
+       ggtitle("Difference: \nLidar avg height - in situ avg height (m)")
 
-
-## ----create-plotly, eval=FALSE-------------------------------------------
-## 
-## library(plotly)
-## 
-## # you must be signed into Plot.ly online on the same computer for this code to work.
-## # generate the plot
-## ggplotly(p,
-##         filename='NEON SJER CHM vs Insitu Tree Height')
-## 
-## 
 
