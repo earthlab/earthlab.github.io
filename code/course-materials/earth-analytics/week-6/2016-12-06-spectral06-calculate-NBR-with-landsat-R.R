e8d60d691e7b8208be78be445aefb37c92ca5584
Leah Wasser <leah.wasser@colorado.edu>
2017-03-16 15:29:54 -0600

    updating week 6

--- code/course-materials/earth-analytics/week-6/2016-12-06-spectral06-calculate-NBR-with-landsat-R.R
+++ code/course-materials/earth-analytics/week-6/2016-12-06-spectral06-calculate-NBR-with-landsat-R.R
@@ -1,4 +1,4 @@
-## ----load-packages, warning=F, message=F, results='hide'-----------------
+## ----load-packages, warning=FALSE, message=FALSE, results='hide'---------
 # load spatial packages
 library(raster)
 library(rgdal)
@@ -17,7 +17,7 @@ all_landsat_bands_pre
 # stack the data
 landsat_stack_pre <- stack(all_landsat_bands_pre)
 
-## ----calculate-nbr, echo=F, fig.cap="landsat derived NDVI plot", fig.width=7, fig.height=5----
+## ----calculate-nbr, echo=FALSE, fig.cap="landsat derived NDVI plot", fig.width=7, fig.height=5----
 # Landsat 8 requires bands 7 and 5
 landsat_nbr_pre <- ((landsat_stack_pre[[5]] - landsat_stack_pre[[7]]) / (landsat_stack_pre[[5]] + landsat_stack_pre[[7]]))
 
@@ -26,7 +26,7 @@ plot(landsat_nbr_pre,
      axes=F,
      box=F)
 
-## ----export-rasters, eval=F----------------------------------------------
+## ----export-rasters, eval=FALSE------------------------------------------
 ## writeRaster(x = landsat_nbr_pre,
 ##               filename="data/week6/outputs/landsat_nbr",
 ##               format = "GTiff", # save as a tif
@@ -42,7 +42,7 @@ all_landsat_bands_post
 # stack the data
 landsat_stack_post <- stack(all_landsat_bands_post)
 
-## ----calculate-nbr-post, echo=F, fig.cap="landsat derived NBR post fire", fig.width=7, fig.height=5----
+## ----calculate-nbr-post, echo=FALSE, fig.cap="landsat derived NBR post fire", fig.width=7, fig.height=5----
 # Landsat 8 requires bands 7 and 5
 landsat_nbr_post <- ((landsat_stack_post[[5]] - landsat_stack_post[[7]]) / (landsat_stack_post[[5]] + landsat_stack_post[[7]]))
 
@@ -74,7 +74,7 @@ nbr_classified <- reclassify(landsat_nbr_diff,
                      reclass_m)
 the_colors = c("seagreen4", "seagreen1", "ivory1", "palevioletred1", "palevioletred4")
 
-## ----classify-output-plot, echo=F, fig.cap="classified NBR output", fig.width=7, fig.height=5----
+## ----classify-output-plot, echo=FALSE, fig.cap="classified NBR output", fig.width=7, fig.height=5----
 # mar bottom, left, top and right
 par(xpd = F, mar=c(0,0,2,5))
 plot(nbr_classified,
@@ -91,14 +91,14 @@ legend(nbr_classified@extent@xmax-100, nbr_classified@extent@ymax,
        bty="n")
 
 
-## ----classify-output-plot2, echo=F, fig.cap="classified NBR output", results='hide', fig.width=7, fig.height=5----
+## ----classify-output-plot2, echo=FALSE, fig.cap="classified NBR output", results='hide', fig.width=7, fig.height=5----
 fire_boundary <- readOGR("data/week6/vector_layers/fire-boundary-geomac/co_cold_springs_20160711_2200_dd83.shp")
 # reproject shapefile
 fire_boundary_utm <- spTransform(fire_boundary, crs(nbr_classified))
 
 
 # mar bottom, left, top and right
-par(xpd = F, mar=c(0,0,2,5))
+par(mar=c(0,0,2,5))
 plot(nbr_classified,
      col=the_colors,
      legend=F,
@@ -107,53 +107,27 @@ plot(nbr_classified,
      main="Landsat NBR - Cold Spring fire site \n Add date of the data here")
 plot(fire_boundary_utm, add=T,
      lwd=5)
-par(xpd = TRUE)
 legend(nbr_classified@extent@xmax-100, nbr_classified@extent@ymax,
        c("Enhanced Regrowth", "Unburned", "Low Severity", "Moderate Severity", "High Severity", "Fire boundary"),
-       col=c(rev(the_colors), "black"),
-       pch=c(15,15, 15, 15, 15,NA),
+       col= "black",
+       pt.bg=rev(the_colors),
+       pch=c(22,22, 22, 22, 22, NA),
        lty = c(NA, NA, NA, NA, NA, 1),
        cex=.8,
        bty="n",
-       pt.cex=c(1.75))
-legend(nbr_classified@extent@xmax-100, nbr_classified@extent@ymax,
-       c("Enhanced Regrowth", "Unburned", "Low Severity", "Moderate Severity", "High Severity", "Fire boundary"),
-       col=c("black"),
-       pch=c(22, 22, 22, 22, 22, NA),
-       lty = c(NA, NA, NA, NA, NA, 1),
-       cex=.8,
-       bty="n",
-       pt.cex=c(1.75))
+       pt.cex=c(1.75),
+       xpd = TRUE)
 
 
-## ----dev-off1, echo=F, warning=F, message=F, results="hide"--------------
+## ----dev-off1, echo=FALSE, warning=FALSE, message=FALSE, results="hide"----
 dev.off()
 
-## ----new-legend, eval=F--------------------------------------------------
-## 
-## legend(nbr_classified@extent@xmax-100, nbr_classified@extent@ymax,
-##        c("Enhanced Regrowth", "Unburned", "Low Severity", "Moderate Severity", "High Severity", "Fire boundary"),
-##        col=c(rev(the_colors), "black"),
-##        pch=c(15,15, 15, 15, 15,NA),
-##        lty = c(NA, NA, NA, NA, NA, 1),
-##        cex=.8,
-##        bty="n",
-##        pt.cex=c(1.75))
-## legend(nbr_classified@extent@xmax-100, nbr_classified@extent@ymax,
-##        c("Enhanced Regrowth", "Unburned", "Low Severity", "Moderate Severity", "High Severity", "Fire boundary"),
-##        col=c("black"),
-##        pch=c(22, 22, 22, 22, 22, NA),
-##        lty = c(NA, NA, NA, NA, NA, 1),
-##        cex=.8,
-##        bty="n",
-##        pt.cex=c(1.75))
-
-## ----classify-output-plot3, echo=F, fig.cap="classified NBR output", fig.width=7, fig.height=5----
+## ----classify-output-plot3, echo=FALSE, fig.cap="classified NBR output", fig.width=7, fig.height=5----
 # look at colors
 # display.brewer.all()
 the_colors <- rev(brewer.pal(5, 'RdYlGn'))
 # mar bottom, left, top and right
-par(xpd = F, mar=c(0,0,2,5))
+par(mar=c(0,0,2,5))
 plot(nbr_classified,
      col=the_colors,
      legend=F,
@@ -162,34 +136,27 @@ plot(nbr_classified,
      main="Landsat NBR - Cold Spring fire site \n Add date of the data here")
      plot(fire_boundary_utm, add=T,
           lwd=5)
-legend(nbr_classified@extent@xmax-100, nbr_classified@extent@ymax,
+legend(nbr_classified@extent@xmax-50, nbr_classified@extent@ymax,
        c("Enhanced Regrowth", "Unburned", "Low Severity", "Moderate Severity", "High Severity", "Fire boundary"),
-       col=c((the_colors), "black"),
-       pch=c(15,15, 15, 15, 15,NA),
+       col= "black",
+       pt.bg = the_colors,
+       pch=c(22, 22, 22, 22, 22, NA),
        lty = c(NA, NA, NA, NA, NA, 1),
        cex=.8,
        bty="n",
        pt.cex=c(1.75),
        xpd=T)
-legend(nbr_classified@extent@xmax-100, nbr_classified@extent@ymax,
-       c("", "", " ", " ", " ", " "),
-       col=c("black"),
-       pch=c(22, 22, 22, 22, 22, NA),
-       lty = c(NA, NA, NA, NA, NA, 1),
-       cex=.8,
-       bty="n",
-       pt.cex=c(1.75))
 
 
-## ----dev-off, echo=F, warning=F, message=F, results="hide", fig.width=7, fig.height=5----
+## ----dev-off, echo=FALSE, warning=FALSE, message=FALSE, results="hide", fig.width=7, fig.height=5----
 dev.off()
 
-## ----view-bar, warning=F, fig.cap="plot barplot of fire severity values"----
+## ----view-bar, warning=FALSE, fig.cap="plot barplot of fire severity values"----
 barplot(nbr_classified,
         main="Distribution of Classified NBR Values",
         col=the_colors)
 
-## ----view-barplot1, warning=F, fig.cap="plot barplot of fire severity values with labels"----
+## ----view-barplot1, warning=FALSE, fig.cap="plot barplot of fire severity values with labels"----
 barplot(nbr_classified,
         main="Distribution of Classified NBR Values",
         col=the_colors,
