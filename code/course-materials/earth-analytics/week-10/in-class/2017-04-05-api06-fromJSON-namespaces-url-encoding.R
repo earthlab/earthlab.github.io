46ae8f9be61e63e1b9e82b8fd99b23f4aa1e5c9b
Leah Wasser <leah.wasser@colorado.edu>
2017-04-12 12:23:01 -0600

    final week 10 redo - holy poo R really 3 fromJSON functions?

--- code/course-materials/earth-analytics/week-10/in-class/2017-04-05-api06-fromJSON-namespaces-url-encoding.R
+++ code/course-materials/earth-analytics/week-10/in-class/2017-04-05-api06-fromJSON-namespaces-url-encoding.R
@@ -27,21 +27,61 @@ pop_proj_data_df <- fromJSON(full_url)
 
 
 ## ------------------------------------------------------------------------
+# view url - notice spaces
+full_url
+# encode spaces with ascii value %20
 full_url <- URLencode(full_url)
 full_url
-# Convert JSON to data frame
+# Convert JSON api returned value to data frame
 pop_proj_data_df <- fromJSON(full_url)
 head(pop_proj_data_df)
 
 ## ------------------------------------------------------------------------
-library(jsonlite)
+library(rjson)
 # Convert JSON to data frame
+# Base URL path
+base_url = "https://data.colorado.gov/resource/tv8u-hswn.json?"
+full_url = paste0(base_url, "county=Boulder",
+             "&$where=age between 20 and 40",
+             "&$select=year,age,femalepopulation")
+# view full url
+full_url
 pop_proj_data_df <- fromJSON(full_url)
 
 ## ------------------------------------------------------------------------
-# encode the URL with characters for each space.
-full_url <- URLencode(full_url)
+# notice the url has spaces
 full_url
-pop_proj_data_df <- fromJSON(full_url)
+
+pop_proj_data_df <- jsonlite::fromJSON(full_url)
+pop_proj_data_df <- rjson::fromJSON(full_url)
+pop_proj_data_df <- RJSONIO::fromJSON(full_url)
+
+## ------------------------------------------------------------------------
+# notice the url has spaces
+full_url
+# encode the url
+full_url_encoded <- URLencode(full_url)
+full_url_encoded
+
+
+pop_proj_data_df <- jsonlite::fromJSON(full_url_encoded)
 head(pop_proj_data_df)
+
+
+## ------------------------------------------------------------------------
+pop_proj_data_df1 <- rjson::fromJSON(full_url_encoded)
+
+## ------------------------------------------------------------------------
+pop_proj_geturl <- getURL(full_url_encoded)
+pop_proj_data_df1 <- rjson::fromJSON(pop_proj_geturl)
+head(pop_proj_data_df1, n=2)
+
+
+## ------------------------------------------------------------------------
+pop_proj_df_convert <- do.call(rbind.data.frame, pop_proj_data_df1)
+head(pop_proj_df_convert)
+
+## ------------------------------------------------------------------------
+pop_proj_data_df2 <- RJSONIO::fromJSON(full_url_encoded)
+head(pop_proj_data_df2, n=2)
 
